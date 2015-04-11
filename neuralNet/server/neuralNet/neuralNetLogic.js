3202648e823596ad6a958017bb88e0312ce18739
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-11 12:46:40 -0700

    adds more extensive comments

--- neuralNet/server/neuralNet/neuralNetLogic.js
+++ neuralNet/server/neuralNet/neuralNetLogic.js
@@ -6,16 +6,19 @@ var brain = require('brain');
 
 //TODO: your code here to create a new neural net instance
 
-
 module.exports = {
+  // this is our main entry point
   startNet: function(req,res) {
-    db.query('SELECT * FROM training', function(err, response) {
+    // grab all the data from the db
+    db.query('SELECT * FROM neuralNet', function(err, response) {
       if(err) {
         console.error(err);
       } else {
+        // format that data. see that modular function below
         var formattedData = module.exports.formatData(response);
         var training = [];
         var testing = [];
+        // split the data into a test set (20% of the data) and a training set (80% of the data)
         for(var i = 0; i < formattedData.length; i++) {
           if(Math.random() > .8) {
             training.push(formattedData[i]);
@@ -23,27 +26,8 @@ module.exports = {
             testing.push(formattedData[i]);
           }
         }
+        // pass this formatted data into trainBrain
         module.exports.trainBrain(training, testing);
-      }
-    });
-  },
-
-  loadAndTestBrain: function(req,res) {
-    db.query('SELECT * FROM training', function(err, response) {
-      if(err) {
-        console.error(err);
-      } else {
-        var formattedData = module.exports.formatData(response);
-        fs.readFile('name', 'utf8', function(err, data) {
-          if(err) {
-            console.error(err);
-          } else {
-            net.fromJSON(JSON.parse(data));
-            res.send('Loaded the brain! Testing it now.')
-            module.exports.testBrain(formattedData);
-          }
-        });
-
       }
     });
   },
@@ -54,13 +38,14 @@ module.exports = {
 
     //TODO: Your code here to train the neural net
 
-    var jsonBackup = net.toJSON();
-    var runBackup = net.toFunction();
-
-    module.exports.writeBrain(jsonBackup);
-
     console.timeEnd('trainBrain');
 
+    // once we've trained the brain, write it to json to back it up
+    var jsonBackup = net.toJSON();
+    var runBackup = net.toFunction();
+    module.exports.writeBrain(jsonBackup);
+
+    // now test the results and see how our machine did!
     module.exports.testBrain(testingData);
   },
 
@@ -71,7 +56,7 @@ module.exports = {
     console.time('testBrain');
     //TODO: Your code here to get the predicted values for everything in our testData
     //The logging code below expects the predicted net values to be stored as properties on each item in testData under the property name output. 
-    //Here's what an object in testData should look like:
+    //Here's what an object in the testData array should look like:
       /*
       { input: 
          { utilizationRate: 0.21939333333333333,
@@ -86,10 +71,12 @@ module.exports = {
         output: { defaulted: 0 },
         nnPredictions: { defaulted: 0.34634397489904356 } }
       */
-    for(var i = 0; i < testData.length; i++) {
-      testData[i].output = net.run(testData[i].input);
-    }
 
+    // everything below is formatting the output
+    // first we create a results obj with keys labeled 0 to 100
+    // eash position in results is an object itself
+      // Each position aggregates the count of loans the neural net has predicted have this level of risk
+      // and the number of observed defaults at that level of risk
     var results = {};
     for(var j = 0; j <=100; j++) {
       results[j] = {
@@ -101,7 +88,7 @@ module.exports = {
     for(var i = 0; i < testData.length; i++) {
       //we format the net's prediction to be a number between 0 and 100
       var prediction = Math.round( testData[i].output.defaulted * 100);
-      //We then increment the total number of cases that the net predicts exist at this level
+      //We then increment the total number of cases that the net predicts exist at this level of risk
       results[prediction].nnCount++;
       //And whether this input resulted in a default or not
       results[prediction].defaulted += testData[i].output.defaulted;
@@ -116,7 +103,52 @@ module.exports = {
     console.log(results);
   },
 
+  //Takes our raw data input, roughly normalizes it, and transforms it into numbers between 0 and 1 like our net expects
+  //You can ignore this until extra credit
+  formatData: function(data) {
+
+    //TODO: SOLUTION CODE BELOW
+    console.log('formatting Data');
+    var formattedResults = [];
+    for(var i = 0; i < data.length; i++) {
+      var item = data[i];
+
+      var obs = {};
+      obs.input = {};
+      obs.output = {
+        defaulted: item.seriousDelinquency
+      };
+
+      //if the utilization rate is below 1, we divide it by 3 to make it smaller (taking the cube root would make it larger);
+      if(item.creditUtilization < 1) {
+        obs.input.utilizationRate = item.creditUtilization/3;
+      } else {
+        //otherwise we take the cube root of it, and then divide by 37 (which is the max number we would have after cube rooting ).
+        obs.input.utilizationRate = Math.pow(item, 1/3)/37;
+      }
+
+      obs.input.age = item.age/109;
+      obs.input.thirtyDaysLate = item['30To60DaysLate'] / 98;
+      obs.input.monthlyIncome = Math.sqrt(item.MonthlyIncome) / 1735;
+      obs.input.openCreditLines = Math.sqrt(item.NumberOfOpenCreditLines)/8;
+
+      obs.input.ninetyDaysLate = Math.sqrt(item['90DaysLate']) / 10;
+
+      obs.input.realEstateLines = item.NumberOfRealEstateLoansOrLines/ 54;
+
+      obs.input.sixtyDaysLate = Math.sqrt(item['60To89DaysLate']) / 10;
+
+      obs.input.numDependents = Math.sqrt(item.NumberOfDependents) / 5;
+
+      formattedResults.push(obs);
+    }
+    console.log('formatted the data');
+    return formattedResults;
+
+  },
+
   //Writes the neural net to a file for backup
+  //You can ignore this 
   writeBrain: function(json) {
     var fileName = 'hiddenLayers' + net.hiddenSizes + 'learningRate' + net.learningRate + new Date().getTime();
     fs.writeFile(fileName, json, function(err) {
@@ -128,48 +160,26 @@ module.exports = {
     });
   },
 
-  //Takes our raw data input, roughly normalizes it, and transforms it into numbers between 0 and 1 like our net expects
-  formatData: function(data) {
-
-    //TODO: refactor to use prettier names in the db
-
-    //TODO: SOLUTION CODE BELOW
-    console.log('formatting Data');
-    var formattedResults = [];
-    for(var i = 0; i < data.length; i++) {
-      var item = data[i];
-
-      var obs = {};
-      obs.input = {};
-      obs.output = {
-        defaulted: item.seriousDlqin2yrs
-      };
-
-      //if the utilization rate is below 1, we divide it by 3 to make it smaller (taking the cube root would make it larger);
-      if(item.revolvingUtilizationOfUnsecuredLines < 1) {
-        obs.input.utilizationRate = item.revolvingUtilizationOfUnsecuredLines/3;
+  // loads a saved neural net and tests it on the dataset
+  // testing it on the entire dataset as we are doing here is an anti-pattern; you would want to test it only on a holdout set of test data that the machine hasn't already been trained on
+  loadAndTestBrain: function(req,res) {
+    db.query('SELECT * FROM neuralNet', function(err, response) {
+      if(err) {
+        console.error(err);
       } else {
-        //otherwise we take the cube root of it, and then divide by 37 (which is the max number we would have after cube rooting ).
-        obs.input.utilizationRate = Math.pow(item, 1/3)/37;
+        var formattedData = module.exports.formatData(response);
+        fs.readFile('brainFileName.txt', 'utf8', function(err, data) {
+          if(err) {
+            console.error(err);
+          } else {
+            net.fromJSON(JSON.parse(data));
+            res.send('Loaded the brain! Testing it now.')
+            module.exports.testBrain(formattedData);
+          }
+        });
+
       }
-
-      obs.input.age = item.age/109;
-      obs.input.thirtyDaysLate = item.NumberOfTime30To59DaysPastDueNotWorse/98;
-      obs.input.monthlyIncome = Math.sqrt(item.MonthlyIncome)/1735;
-      obs.input.openCreditLines = Math.sqrt(item.NumberOfOpenCreditLinesAndLoans)/8;
-
-      obs.input.ninetyDaysLate = Math.sqrt(item.NumberOfTimes90DaysLate)/10;
-
-      obs.input.realEstateLines = item.NumberRealEstateLoansOrLines/54;
-
-      obs.input.sixtyDaysLate = Math.sqrt(item.NumberOfTime60To89DaysPastDueNotWorse)/10;
-
-      obs.input.numDependents = Math.sqrt(item.NumberOfDependents)/5;
-
-      formattedResults.push(obs);
-    }
-    console.log('formatted the data');
-    return formattedResults;
-
+    });
   }
+
 };
