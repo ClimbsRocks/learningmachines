77f0966782a57af3c3269d9c9dc2217222bbabd7
ClimbsRocks <ClimbsRocks@users.noreply.github.com>
2015-04-11 11:59:45 -0700

    points MySQL to the updated db

--- neuralNet/server/db/index.js
+++ neuralNet/server/db/index.js
@@ -2,9 +2,7 @@ var mysql = require('mysql');
 
 var dbConnection = mysql.createConnection({
   user: 'root',
-  database: 'nnCredit'
+  database: 'learningMachines'
 });
-
-// dbConnection.connect();
 
 module.exports = dbConnection;
