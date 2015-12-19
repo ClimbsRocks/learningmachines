aa3c5bdb63945e905cb59ca52e6c9a733abac9c8
ClimbsRocks <climbsbytes@gmail.com>
2015-12-19 14:06:04 -0800

    slightly rewords comments for solution code

--- neuralNet/server/neuralNet/neuralNetLogic.js
+++ neuralNet/server/neuralNet/neuralNetLogic.js
@@ -5,11 +5,12 @@ var fs = require('fs');
 var brain = require('brain');
 
 //TODO: your code here to create a new neural net instance
-// SOLUTION CODE BELOW
+// START SOLUTION CODE
 var net = new brain.NeuralNetwork({
   hiddenLayers:[20,20], //Use the docs to explore various numbers you might want to use here
   learningRate:0.6
 });
+//  END SOLUTION CODE
 
 module.exports = {
   // this is our main entry point
@@ -42,7 +43,7 @@ module.exports = {
     console.log('Training your very own Brain');
 
     //TODO: Your code here to train the neural net
-    // SOLUTION CODE BELOW:
+    // START SOLUTION CODE:
     var writeInfo = net.train(trainingData,{
       errorThresh: 0.05,  // error threshold to reach
       iterations: 10,   // maximum training iterations
@@ -50,6 +51,8 @@ module.exports = {
       logPeriod: 1,       // number of iterations between logging
       learningRate: 0.3    // learning rate
     });
+
+    //  END SOLUTION CODE
 
     console.timeEnd('trainBrain');
 
@@ -85,10 +88,11 @@ module.exports = {
         nnPredictions: { defaulted: 0.34634397489904356 } }
       */
 
-    // SOLUTION CODE BELOW
+    // START SOLUTION CODE
     for(var i = 0; i < testData.length; i++) {
       testData[i].nnPredictions = net.run(testData[i].input);
     }
+    // END SOLUTION CODE
 
     // everything below is formatting the output
     // first we create a results obj with keys labeled 0 to 100
