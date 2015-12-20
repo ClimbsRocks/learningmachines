4f9f57488974755f551cf30e9d163111986aeece
ClimbsRocks <climbsbytes@gmail.com>
2015-12-19 18:32:04 -0800

    fixes script to strip solution code

--- stripSolutionCode.sh
+++ stripSolutionCode.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# ALL HTML FILES
-FILES="*.js"
-# for loop read each file
+# all .js files, exclusing the node_modules folder
+FILES=`find . -path ./neuralNet/server/node_modules -prune -o -name "*.js"`
+
+# for loop to read each file
 for f in $FILES
 do
-INF="$f"
-OUTF="$f.out.tmp"
-# replace javascript
-sed '/START SOLUTION CODE/,/END SOLUTION CODE/d' $INF > $OUTF
-/bin/cp $OUTF $INF
-/bin/rm -f $OUTF
+  # remove (in place with the -i '' flag that only works on os x) everything in between the solution tags, inclusive of the solution tag lines themselves
+  sed -i '' '/START SOLUTION CODE/,/END SOLUTION CODE/d' $f
 done
