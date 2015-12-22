e7a64db9ce4c2bd4e86b8a4b8ff00c580a5df72c
ClimbsRocks <climbsbytes@gmail.com>
2015-12-22 10:51:23 -0500

    explains intentionally verbose Router setup- easier to read

--- neuralNet/server/neuralNet/neuralNetRouter.js
+++ neuralNet/server/neuralNet/neuralNetRouter.js
@@ -3,6 +3,10 @@ var express = require('express');
 var neuralNetRouter = express.Router();
 var neuralNetLogic = require('./neuralNetLogic.js');
 
+// this could certainly be done more simply, but I actually prefer laying each one out individually, as we do here
+// this allows us to see very directly which methods we expect to be publicly available
+// and also allows us to add in some logging for the user to see
+
 neuralNetRouter.get('/formatData', function(req, res) {
   console.log('heard a request to formatData');
   neuralNetLogic.formatData(req, res);
