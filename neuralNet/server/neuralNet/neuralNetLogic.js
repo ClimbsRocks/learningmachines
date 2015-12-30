09f916f4698f1936b36a14a8f826b0200c53af76
ClimbsRocks <climbsbytes@gmail.com>
2015-12-30 09:21:34 -0800

    parses csv file data and adds a comment explaining what we're doing

--- neuralNet/server/neuralNet/neuralNetLogic.js
+++ neuralNet/server/neuralNet/neuralNetLogic.js
@@ -17,13 +17,16 @@ module.exports = {
   // this is our main entry point
   startNet: function(req,res) {
 
-    fs.readFile('train.csv', 'utf8', function(err, data) {
+    fs.readFile('train.csv', 'utf8', function(err, fileData) {
       if(err) {
         console.error('error reading in the train.csv file. Please make sure it is saved in the same directory as neuralNetLogic.js, and is named train.csv');
         console.error(err);
       } else {
+        // csv files can be saved in a number of different ways. PapaParse (with it's node.js version BabyParse) will take care of all the messiness for us, and reliably return data in a consistent format for us to work with.
+        rows = Baby.parse(fileData).data;
+        console.low(rows);
         // format that data. see that modular function below
-        var formattedData = module.exports.formatData(response);
+        var formattedData = module.exports.formatData(rows);
         var training = [];
         var testing = [];
         // split the data into a test set (20% of the data) and a training set (80% of the data)
@@ -41,26 +44,26 @@ module.exports = {
 
     // The code below only applies to people who wanted to get in more practice with databases. 
     // grab all the data from the db
-    db.query('SELECT * FROM neuralNet', function(err, response) {
-      if(err) {
-        console.error(err);
-      } else {
-        // format that data. see that modular function below
-        var formattedData = module.exports.formatData(response);
-        var training = [];
-        var testing = [];
-        // split the data into a test set (20% of the data) and a training set (80% of the data)
-        for(var i = 0; i < formattedData.length; i++) {
-          if(Math.random() > .8) {
-            testing.push(formattedData[i]);
-          } else {
-            training.push(formattedData[i]);
-          }
-        }
-        // pass this formatted data into trainBrain
-        module.exports.trainBrain(training, testing);
-      }
-    });
+    // db.query('SELECT * FROM neuralNet', function(err, response) {
+    //   if(err) {
+    //     console.error(err);
+    //   } else {
+    //     // format that data. see that modular function below
+    //     var formattedData = module.exports.formatData(response);
+    //     var training = [];
+    //     var testing = [];
+    //     // split the data into a test set (20% of the data) and a training set (80% of the data)
+    //     for(var i = 0; i < formattedData.length; i++) {
+    //       if(Math.random() > .8) {
+    //         testing.push(formattedData[i]);
+    //       } else {
+    //         training.push(formattedData[i]);
+    //       }
+    //     }
+    //     // pass this formatted data into trainBrain
+    //     module.exports.trainBrain(training, testing);
+    //   }
+    // });
   },
 
   trainBrain: function(trainingData, testingData) {
