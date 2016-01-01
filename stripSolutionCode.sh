050c78463b92be4b04230b5f47b06efb385c23c8
ClimbsRocks <climbsbytes@gmail.com>
2015-12-31 17:27:59 -0800

    adds stripSolutionScript to solution branch

---
+++ stripSolutionCode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gch master
+git merge solution
+# all .js files, exclusing the node_modules folder
+FILES=`find . -path ./neuralNet/server/node_modules -prune -o -name "*.js"`
+
+# for loop to read each file
+for f in $FILES
+do
+  # remove (in place with the -i '' flag that only works on os x) everything in between the solution tags, inclusive of the solution tag lines themselves
+  sed -i '' '/START SOLUTION CODE/,/END SOLUTION CODE/d' $f
+done
