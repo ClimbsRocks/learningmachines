eba7ec086853e14e959800c98969e324cba54712
ClimbsRocks <climbsbytes@gmail.com>
2016-01-05 17:40:49 -0800

    performs more feature engineering, and reads in values as floats where possible

--- random_forest/random_forest.py
+++ random_forest/random_forest.py
@@ -25,9 +25,9 @@ with open('titanic.csv', 'rU') as openInputFile:
     for row in inputRows:
         if(firstRow):
             # if possible, read in the data as floats (numbers with decimal points) rather than strings
-            for key, val in enumerate(row):
+            for key in row:
                 try:
-                    row[key] = float(val)
+                    row[key] = float(row[key])
                 except:
                     pass
             X.append(row)
@@ -35,23 +35,16 @@ with open('titanic.csv', 'rU') as openInputFile:
             headerRow = row
             firstRow = True
 
-# transform:
-    # name
-    # sex
-    # ticket
-        # take only the letters in front of ticket. categorize that
-        # if there is a good ticket number, include that
-
-    # cabin
-        # definitely parse out the leading letter here. that denotes the deck they're on. 
-        # possibly parse out the number of cabins they have as well?
-    # embarked
 # TODO: binarize the categorical data in the categorical fields
 # information like 'embarked' represents a category (which city did you embark in?) rather than a number (how much money did you pay for your fare?)
 # a common way to format categorical data for machine learning is to turn it into binary values
 # so if we have three categories ('C','Q',and 'S'), we would turn that into something like separate columns for 'embarkedC', 'embarkedQ', and 'embarkedS', with a value of either 0 or 1
 # define your own binarize function here, that will take in categorical data, and turn it into a single binary representation of which category is present for this row
 def binarize(columnName, columnValue, passengerObj):
+    try:
+        columnValue = str(columnValue)
+    except:
+        pass
     keyName = columnName + columnValue
     passengerObj[keyName] = 1
     return passengerObj
@@ -61,15 +54,28 @@ cleanedX = []
 for row in X:
     # {'Fare': '7.75', 'Name': 'Dooley, Mr. Patrick', 'Embarked': 'Q', 'Age': '32', 'Parch': '0', 'Pclass': '3', 'Sex': 'male', 'Survived': '0', 'SibSp': '0', 'PassengerId': '891', 'Ticket': '370376', 'Cabin': ''}
 
-
     # split out our y values (these are the answers we're looking for- in our case, whether this person survived or not)
     y.append(row['Survived'])
     del row['Survived']
 
+    row['totalConnections'] = row['Parch'] + row['SibSp']
+    row = binarize('Pclass', row['Pclass'], row)
+    row = binarize('SibSp', row['SibSp'], row)
+    row = binarize('Parch', row['Parch'], row)
+
+    # if the cabin is known, grab the first letter from it, which might represent something useful like which deck they're on
+    try:
+        row['cabinDeck'] = row['Cabin'][0]
+        row['hasAssignedCabin'] = 1
+    except:
+        row['hasAssignedCabin'] = 0
+        pass
+    row['TicketFirstChar'] = str(row['Ticket'])[0]
     # TODO: figure out why we keep getting key errors when deleting survived from a row
 
     cleanedX.append(row)
 # END SOLUTION CODE
+
 
 vectorizedX = vectorizer.fit_transform(cleanedX)
 
@@ -78,5 +84,6 @@ classifier.fit( X_train, y_train )
 
 # NOTE: if your score is really high (say, over 0.85), check to make sure you removed the answer from the information you gave the random forest to train on
 # otherwise it's simply going to learn that the 'Survived' column you fed it is HIGHLY correlated with the answer
+print 'Your random forest\'s score on the test data is:'
 print classifier.score( X_test, y_test )
 
